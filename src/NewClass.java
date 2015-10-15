3b985d910787ee285ab594bfee758a3a3887b555
tomimediasahr <tomimediasahr@yahoo.co.id>
2015-10-15 12:27:51 +0700

    Versi Satu

---
+++ src/NewClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sss
+ */
+public class NewClass {
+     public int nilaiHasil;
+    
+    int penjumlahan (int a, int b){
+        nilaiHasil = a+b;
+        return nilaiHasil;
+    }
+    int pengurangan (int a, int b){
+        nilaiHasil = a-b;
+        return nilaiHasil;
+    }
+    int perkalian (int a, int b){
+        nilaiHasil = a*b;
+        return nilaiHasil;
+    }
+    float pembagian (float a, float b){
+        float Hasil = (a / b);
+        return Hasil;
+    }   
+    
+}
